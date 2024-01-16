2797416d429efbf1131653c86372a8a1604fd590
secretval <108518296+secretval@users.noreply.github.com>
2024-01-16 20:36:35 +0100

    working

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 mod tui;
 
-use std::{fs, env};
 use std::process::Command;
+use std::{env, fs};
 
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = r#"
+#[command(
+    author,
+    version,
+    about,
+    long_about = r#"
           This is a cli tool to create/init new new projects.
 
           Special thanks to:
           ALT-F4-LLC (creator of )https://github.com/ALT-F4-LLC/kickstart.nix ☺
-"#)]
+"#
+)]
 #[command(propagate_version = true)]
 struct Cli {
     #[command(subcommand)]
@@ -25,77 +30,78 @@ enum Commands {
 }
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = "Create a project using kickstart.nix")]
-struct CreateArgs{
-    #[arg(short, long,required=false,requires="language")]
+#[command(
+    author,
+    version,
+    about,
+    long_about = "Create a project using kickstart.nix"
+)]
+struct CreateArgs {
+    #[arg(short, long, required = false, requires = "language")]
     dir: Option<String>,
 
-    #[arg(short, long,required=false,requires="dir")]
+    #[arg(short, long, required = false, requires = "dir")]
     language: Option<String>,
 }
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = "Initialize a project using kickstart.nix")]
-struct InitArgs{
-    #[arg(short, long,required=false,requires="dir")]
+#[command(
+    author,
+    version,
+    about,
+    long_about = "Initialize a project using kickstart.nix"
+)]
+struct InitArgs {
+    #[arg(short, long, required = false, requires = "dir")]
     language: Option<String>,
 }
 
 fn main() {
     let cli = Cli::parse();
     match &cli.command {
-        Commands::Create(args) => {
-            match &args.dir {
-                Some(dir) => {
-                    match &args.language {
-                        Some(language) => {
-                            fs::create_dir(&dir)
-                                .expect("couldnt create directory");
-                            let _ = std::env::set_current_dir(dir)
-                                .expect("Couldn't go into directory");
-                            let _ = Command::new("nix")
-                                .arg("flake")
-                                .arg("init")
-                                .arg("-t")
-                                .arg(format!("github:ALT-F4-LLC/kickstart.nix#{}",language))
-                                .output();
-
-
-                            let mut flake = fs::read_to_string("./flake.nix").unwrap();
-                            flake = flake.replace("example", &dir);
-                            let _ = fs::write("./flake.nix", flake).unwrap();
-                        },
-                        None => {},
-                    }
-                },
-                None => {
-                    let _ = tui::create().map_err(|err|{
-                        eprintln!("There was this error: {:?}", err);
-                    });
-                },
-            }
-        },
-        Commands::Init(args) => {
-            match &args.language {
+        Commands::Create(args) => match &args.dir {
+            Some(dir) => match &args.language {
                 Some(language) => {
+                    fs::create_dir(&dir).expect("couldnt create directory");
+                    let _ = std::env::set_current_dir(dir).expect("Couldn't go into directory");
                     let _ = Command::new("nix")
                         .arg("flake")
                         .arg("init")
                         .arg("-t")
-                        .arg(format!("github:ALT-F4-LLC/kickstart.nix#{}",language))
+                        .arg(format!("github:ALT-F4-LLC/kickstart.nix#{}", language))
                         .output();
-                    let current_dir = env::current_dir().unwrap();
-                    let binding = current_dir.display().to_string();
-                    let dir = binding.split("/").last().unwrap();
 
                     let mut flake = fs::read_to_string("./flake.nix").unwrap();
                     flake = flake.replace("example", &dir);
                     let _ = fs::write("./flake.nix", flake).unwrap();
-                },
-                None => {
-                    let _ = tui::init().map_err(|err| {
-                        eprintln!("There was this error: {:?}", err);
-                    });
-                },
+                }
+                None => {}
+            },
+            None => {
+                let _ = tui::create().map_err(|err| {
+                    eprintln!("There was this error: {:?}", err);
+                });
+            }
+        },
+        Commands::Init(args) => match &args.language {
+            Some(language) => {
+                let _ = Command::new("nix")
+                    .arg("flake")
+                    .arg("init")
+                    .arg("-t")
+                    .arg(format!("github:ALT-F4-LLC/kickstart.nix#{}", language))
+                    .output();
+                let current_dir = env::current_dir().unwrap();
+                let binding = current_dir.display().to_string();
+                let dir = binding.split("/").last().unwrap();
+
+                let mut flake = fs::read_to_string("./flake.nix").unwrap();
+                flake = flake.replace("example", &dir);
+                let _ = fs::write("./flake.nix", flake).unwrap();
+            }
+            None => {
+                let _ = tui::init().map_err(|err| {
+                    eprintln!("There was this error: {:?}", err);
+                });
             }
         },
     }
