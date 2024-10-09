a7962982aa1b40b6a895e4be483b63a32a9bd74b
secretval <108518296+SecretVal@users.noreply.github.com>
2024-10-09 20:00:33 +0200

    remove overlay

--- flake.nix
+++ flake.nix
@@ -3,15 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      imports = [
-        inputs.flake-parts.flakeModules.easyOverlay
-      ];
 
       perSystem = {
         config,
@@ -32,10 +28,6 @@
           default = pkgs.mkShell {
             inputsFrom = [self'.packages.default];
           };
-        };
-
-        overlayAttrs = {
-            inherit (config.packages) default;
         };
 
         packages = {
